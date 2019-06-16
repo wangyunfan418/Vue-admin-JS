af1257b50cb8e2aa0fc85c6e0d65343bc3473ee1
MARS <marshal_max@163.com>
2019-06-16 23:14:47 +0800

    :boom: 表单验证封装中

---
+++ src/components/FormRules/formRules.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <!-- <el-row :gutter="30">
+    <el-col :span="this.span">-->
+    <el-form :model="form">
+      <el-form-item v-for="item in formData" :key="item.key" :label="item.name" :prop="item.key">
+        <el-input
+          v-if="item.type === 'text'"
+          v-model="form[item.key]"
+          :size="item.size"
+          clearable
+          :maxlength="item.maxlength"
+          :minlenght="item.minlength"
+        />
+        <el-date-picker
+          v-if="item.type === 'date'"
+          v-model="form[item.key]"
+          type="date"
+          placeholder="选择日期"
+          :size="item.size"
+          style="width: 100%;"
+          :maxlength="item.maxlength"
+          :minlenght="item.minlength"
+        />
+        <el-select
+          v-if="item.type === 'select'"
+          v-model="form[item.key]"
+          placeholder="请选择"
+          :size="item.size"
+          clearable
+          style="width: 100%;"
+          :maxlength="item.maxlength"
+          :minlenght="item.minlength"
+        >
+          <el-option
+            v-for="select in newArr"
+            :key="select.id"
+            :label="select.label"
+            :value="select.value"
+          />
+        </el-select>
+        <el-input
+          v-if="item.type === 'textarea'"
+          type="textarea"
+          v-model="form[item.key]"
+          style="width: 100%;"
+          :maxlength="item.maxlength"
+          :minlenght="item.minlength"
+        ></el-input>
+      </el-form-item>
+      <el-button v-show="formSet.submitShow" type="primary" size="mini" @click="onSubmit">保存</el-button>
+      <el-button v-show="formSet.cancelShow" size="mini" @click="onCancel">取消</el-button>
+    </el-form>
+    <!-- </el-col>
+    </el-row>-->
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    span: {
+      type: Number,
+      default: ''
+    },
+    form: {
+      type: Object,
+      default: () => {}
+    },
+    formData: {
+      type: Array,
+      default: () => []
+    },
+    formSet: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      newArr: []
+    }
+  },
+  mounted() {
+    console.log(this.span)
+    // !判断formData数组中是否存在数组selects
+    this.formData.map((item) => {
+      if (!item.selects) {
+        return
+      } else {
+        this.newArr = item.selects
+      }
+      console.log(this.newArr)
+    })
+  },
+  methods: {
+    // !确定按钮点击事件
+    onSubmit() {
+      console.log('确定按钮点击')
+    },
+    // !取消按钮点击事件
+    onCancel() {
+      console.log('取消按钮点击')
+    }
+  }
+}
+</script>
+
--- src/views/nested/menu1/menu1-1/index.vue
+++ src/views/nested/menu1/menu1-1/index.vue
@@ -1,7 +1,110 @@
 <template>
-  <div style="padding:30px;">
-    <el-alert :closable="false" title="menu 1-1" type="success">
-      <router-view />
-    </el-alert>
+  <div class="form-style">
+    <form-rules
+      :span="span"
+      :form="form"
+      :form-data="formData"
+      :form-set="formSet"
+      @subMit="subMit"
+    />
   </div>
 </template>
+<script>
+import formRules from '@/components/FormRules/formRules'
+export default {
+  components: {
+    formRules
+  },
+  data() {
+    return {
+      form: {},
+      // ?表单配置项
+      formSet: {
+        isFilterBar: false,
+        isSeach: false,
+        isExport: false,
+        isImport: false,
+        // ?确定按钮
+        submitShow: true,
+        // ?取消按钮
+        cancelShow: true
+      },
+      // ?网格
+      span: 12,
+      // ?数据
+      formData: [
+        {
+          key: 'name',
+          trigger: 'blur',
+          name: '姓名',
+          width: '80px',
+          maxlength: 20,
+          minlength: 0,
+          type: 'text',
+          size: 'mini',
+          validator: ['isNotEmpty', '*maxLengh?5']
+        }, {
+          key: 'time',
+          trigger: 'blur',
+          name: '开始时间',
+          width: '80px',
+          maxlength: 2,
+          minlength: 0,
+          type: 'date',
+          size: 'small ',
+          validator: ['isNotEmpty', '*maxLengh?5']
+        }, {
+          key: 'address',
+          trigger: 'blur',
+          name: '地址',
+          width: '80px',
+          maxlength: 20,
+          minlength: 0,
+          type: 'select',
+          size: 'small ',
+          validator: ['isNotEmpty', '*maxLengh?5'],
+          selects: [
+            {
+              id: '1',
+              label: 'China',
+              value: 'China'
+            }, {
+              id: '2',
+              label: 'US',
+              value: 'US'
+            }, {
+              id: '3',
+              label: 'UK',
+              value: 'UK'
+            }
+          ]
+        }, {
+          key: 'remake',
+          trigger: 'blur',
+          name: '备注',
+          width: '80px',
+          maxlength: 200,
+          minlength: 0,
+          type: 'textarea'
+        }
+      ]
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+    subMit() {
+      console.log(111)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.form-style {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+}
+</style>
+
